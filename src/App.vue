9eb4c7d00c833ab26e22643fb708a5b584aae383
Joe Lewis <lewis.joe.18@gmail.com>
2019-04-19 19:44:42 +0530

    searchbar with mutliple selects in working contiion

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-toolbar dark>
+        <v-toolbar-title>
+          <span class="logo-post-text">CODE</span><span class="logo-pre-text">BEAT</span>
+        </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+
+    <v-content>
+      <v-container>
+        <v-layout>
+          <v-flex>
+            <search-bar v-model="projects"></search-bar>
+          </v-flex>
+        </v-layout>
+        <v-layout>
+          <v-flex>
+            <commit-graph :projects="projects"></commit-graph>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import SearchBar from './components/SearchBar'
+import CommitGraph from './components/CommitGraph'
 
 export default {
-  name: "app",
+  name: 'App',
   components: {
-    HelloWorld
+    SearchBar,
+    CommitGraph
+  },
+  data () {
+    return {
+      projects: [],
+      examples: [
+        {label: 'Frontend Frameworks', projects: ['facebook/react', 'google/angular']},
+        {label: 'Go Web Frameworks', projects: ['gorilla/gorilla']}
+      ]
+    }
   }
-};
+}
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.logo-pre-text {
+  font-weight: 400;
 }
+.logo-post-text {
+  font-weight: 900;
+}
+
 </style>
---
+++ src/components/CommitGraph.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        Here goes the commit graphs for {{ this.projects.map(function(project) { return project.name }) }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        projects: {
+            type: Array,
+            default: []
+        }
+    },
+
+
+}
+</script>
